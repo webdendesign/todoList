9bf160c401cb3948f9dd275c91ba5d776a522dbf
Den <fabri.din@yandex.ru>
2021-07-05 20:11:54 +0500

    event handling

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,6 +3,10 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+  onLabelClick = () => {
+    console.log(`Done: ${this.props.label}`);
+  };
+
   render() {
 
     const {label, important = false } =  this.props;
@@ -16,7 +20,8 @@ export default class TodoListItem extends Component {
       <span className="todo-list-item">
         <span
           className="todo-list-item-label"
-          style={style}>
+          style={style}
+          onClick={ this.onLabelClick }>
           {label}
         </span>
 
@@ -31,5 +36,5 @@ export default class TodoListItem extends Component {
         </button>
       </span>
     )
-  }
-}
+  };
+};
