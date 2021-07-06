55e9c09649ddb3c8ffce9b95f966b8aca4ce5c5f
Den <fabri.din@yandex.ru>
2021-07-06 15:07:04 +0500

    filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,8 @@ export default class App extends Component {
       this.createTodoItem('React'),
       this.createTodoItem('App'),
     ],
-    term: ''
+    term: '',
+    filter: 'all'
   };
 
   createTodoItem(label) {
@@ -94,6 +95,10 @@ export default class App extends Component {
     this.setState({term});
   };
 
+  onFilterChange = (filter) => {
+    this.setState({filter});
+  };
+
   search(items, term) {
     if(term.length === 0) {
       return items;
@@ -104,11 +109,25 @@ export default class App extends Component {
     });
   };
 
+  filter(items, filter) {
+
+    switch(filter) {
+      case 'all':
+        return items;
+      case 'active':
+        return items.filter((item) => !item.done);
+      case 'done' :
+        return items.filter((item) => item.done);
+      default:
+        return items;
+    };
+  };
+
   render() {
 
-    const {todoData, term} = this.state;
+    const {todoData, term, filter} = this.state;
 
-    const visibleItems = this.search(todoData, term);
+    const visibleItems = this.filter(this.search(todoData, term), filter) ;
 
     const doneCount = todoData.filter((el) => el.done).length;
 
@@ -119,8 +138,10 @@ export default class App extends Component {
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
           <SearchPanel
-            onSearchChange={this.onSearchChange}/>
-          <ItemStatusFilter />
+            onSearchChange={this.onSearchChange} />
+          <ItemStatusFilter
+            filter={filter}
+            onFilterChange={this.onFilterChange} />
         </div>
 
         <TodoList
@@ -129,7 +150,7 @@ export default class App extends Component {
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
         />
-        <ItemAddForm onItemAdded={this.addItem}/>
+        <ItemAddForm onItemAdded={this.addItem} />
       </div>
     );
   };
